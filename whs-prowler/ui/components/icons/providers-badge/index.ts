def7886c37e9f7a1c98c550c1776010b00868ea4
sksdudals99 <skdudals99@gmail.com>
2025-07-25 20:19:42 +0900

    Add terraform folder under structure/

--- whs-prowler/ui/components/icons/providers-badge/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./aws-provider-badge";
-export * from "./azure-provider-badge";
-export * from "./gcp-provider-badge";
-export * from "./ks8-provider-badge";
-export * from "./m365-provider-badge";
--- whs-prowler/ui/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./authFormSchema";
-export * from "./components";
-export * from "./filters";
-export * from "./formSchemas";
-export * from "./providers";
-export * from "./scans";
